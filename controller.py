f3f22f81876bee3cf5ed6fcdf6c9bc86fcac83a4
Dariia Shumeiko <dariia.shumeiko@student.tuke.sk>
2024-11-26 09:09:00 +0100

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,11 @@
+
+import tuke_openlab
+
+openlab = tuke_openlab.Controller(tuke_openlab.production_env())
+
+openlab.screens.panel_2x2.set_url("https://pranx.com/hacker/")
+
+for i in range(1,6):
+    openlab.screens.vertical.get(i).set_url("https://pranx.com/hacker/")
+
+
