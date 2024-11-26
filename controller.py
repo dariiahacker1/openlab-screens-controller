c0136528b599073ef383a36393422a989c8357f8
Dariia Shumeiko <dariia.shumeiko@student.tuke.sk>
2024-11-26 15:17:18 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,19 +7,21 @@ openlab = tuke_openlab.Controller(tuke_openlab.production_env())
 # prank screens
 screens = ["https://pranx.com/hacker/", "https://pranx.com/matrix-code-rain/", "https://pranx.com/fake-virus/"]
 
-def reset_to_default():
-        try:
-            openlab.screens.panel_2x2.set_default()
-            for i in range(1, 6):
-                openlab.screens.vertical.get(i).set_default()
 
-        except Exception as e:
-            print(f"Error resetting screens: {e}")
+def reset_to_default():
+    try:
+        openlab.screens.panel_2x2.set_default()
+        for i in range(1, 6):
+            openlab.screens.vertical.get(i).set_default()
+
+    except Exception as e:
+        print(f"Error resetting screens: {e}")
+
 
 def set_screens(url_index):
     try:
         openlab.screens.panel_2x2.set_url(screens[url_index])
-        for i in range(1,6):
+        for i in range(1, 6):
             openlab.screens.vertical.get(i).set_url(screens[url_index])
         print(f"Screens set to: {screens[url_index]}")
     except IndexError:
@@ -27,6 +29,7 @@ def set_screens(url_index):
 
     except Exception as e:
         print(f"Error setting screens: {e}")
+
 
 def main():
     if len(sys.argv) < 2:
@@ -38,6 +41,7 @@ def main():
             set_screens(int(sys.argv[1]) - 1)
         except ValueError:
             print("Invalid argument. Please provide an integer.")
+
 
 if __name__ == "__main__":
     main()
